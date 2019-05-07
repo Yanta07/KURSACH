f91a25f9f7cb0806a38f723cb1191242b6225405
SLSNe <barara54@hotmail.com>
2019-05-07 17:04:59 +0700

    Added PVS studio header for free use

--- Calculator/Parser/AST/VariableExprAST.cs
+++ Calculator/Parser/AST/VariableExprAST.cs
@@ -1,4 +1,6 @@
-﻿using calculator.Parser.Context;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+using calculator.Parser.Context;
 
 namespace calculator.Parser.AST
 {
