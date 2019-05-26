8387648b510041276607d84dc364f37d271fdddc
SLSNe <barara54@hotmail.com>
2019-05-26 22:14:03 +0700

    Reverse YRoot function parameters

--- Calculator/CalcMathematics/Math.cs
+++ Calculator/CalcMathematics/Math.cs
@@ -7,7 +7,17 @@ namespace calculator.CalcMathematics
     {
         public static double YRoot(double a, double b)
         {
-            return Math.Round(Math.Pow(Math.Abs(a), 1.0 / b) * Math.Sign(a));
+            return Math.Round(Math.Pow(Math.Abs(b), 1.0 / Math.Abs(a)) * Math.Sign(a));
+        }
+
+        public static double Fact(double number)
+        {
+            return Enumerable.Range(1, Convert.ToInt32(number)).Aggregate((i, r) => r * i);
+        }
+
+        public static double Root(double number)
+        {
+            return Math.Sqrt(number);
         }
     }
 }
