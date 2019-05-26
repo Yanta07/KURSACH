7abddddd002087a0485da9d8b556375f41aa6dc2
SLSNe <barara54@hotmail.com>
2019-05-26 22:22:49 +0700

    Added unit test to calcMath class

---
+++ CalculatorUnitTests/CalcMathTest.cs
@@ -0,0 +1,30 @@
+﻿using calculator.CalcMathematics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorUnitTests
+{
+    [TestClass]
+    public class CalcMathTest
+    {
+        [TestMethod]
+        public void YRootTest()
+        {
+            Assert.AreEqual(CalcMath.YRoot(2, 4), 2);
+            Assert.AreEqual(CalcMath.YRoot(4, 256), 4);
+        }
+
+        [TestMethod]
+        public void RootTest()
+        {
+            Assert.AreEqual(CalcMath.Root(4), 2);
+            Assert.AreEqual(CalcMath.Root(256), 16);
+        }
+
+        [TestMethod]
+        public void FactorialTest()
+        {
+            Assert.AreEqual(CalcMath.Fact(5), 120);
+            Assert.AreEqual(CalcMath.Fact(4), 24);
+        }
+    }
+}
