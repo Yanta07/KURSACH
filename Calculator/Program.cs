90dc800985854a39831ed73dcbb23901f32e9c69
SLSNe <barara54@hotmail.com>
2019-05-30 19:37:39 +0700

    Removed unused using spaces

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,9 +1,6 @@
 ﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using calculator.Form;
 
